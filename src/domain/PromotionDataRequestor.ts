2f94c576cb42b224a398399e663e63debda8fe6e
Abmael <abmael.cont@gmail.com>
2022-08-10 19:38:23 -0300

    feat: method to request all available promotions

--- src/domain/PromotionDataRequestor.ts
+++ src/domain/PromotionDataRequestor.ts
@@ -1,5 +1,15 @@
-class PromotionDataRequestor {
+import { Promotion } from './Promotion'
+import { fetch } from './http_client/fetch'
 
+// TODO: What will it return when some error ocurr ?
+class PromotionDataRequestor {
+    private URL = 'http://www.mocky.io/v2/5bc3b9cc30000012007586b7';
+
+    async requestAll(): Promise<Promotion[]> {
+        const { data: promotions } = await fetch<Promotion[]>(this.URL);
+        
+        return promotions;
+    }
 }
 
 export { PromotionDataRequestor };
