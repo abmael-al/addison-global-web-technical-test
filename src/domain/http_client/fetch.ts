c4d32db50caf93ec7c2b817baddbe3f53a436922
Abmael <abmael.cont@gmail.com>
2022-08-10 19:36:37 -0300

    feat: fetch HTTP client

--- src/domain/http_client/fetch.ts
+++ src/domain/http_client/fetch.ts
@@ -1,5 +1,9 @@
-function fetch() {
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
+async function fetch<T = unknown>(url: string, config?: AxiosRequestConfig): Promise<AxiosResponse<T>> {
+    const response = await axios.get<T>(url, config);
+
+    return response;
 }
 
 export { fetch };
