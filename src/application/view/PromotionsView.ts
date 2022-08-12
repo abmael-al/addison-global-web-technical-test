de8a708df86a122aff2780a583dab03f93c29623
Abmael <abmael.cont@gmail.com>
2022-08-11 21:52:56 -0300

    feat: promotions view

--- src/application/view/PromotionsView.ts
+++ src/application/view/PromotionsView.ts
@@ -1,5 +1,21 @@
-class PromotionsView {
+import { Promotion } from "../../domain/Promotion";
 
+class PromotionsView {
+    private readonly PromotionsRequestor;
+    private readonly PromotionsRoot;
+
+    constructor(PromotionsRequestor: any, PromotionsRoot: any) {
+        this.PromotionsRequestor = PromotionsRequestor;
+        this.PromotionsRoot = PromotionsRoot;
+    }
+    
+    async start() {
+        const promotions: Promotion[] = await this.PromotionsRequestor.requestAll();
+
+        for(const promotion of promotions){
+            this.PromotionsRoot.render(promotion);
+        }
+    }
 }
 
 export { PromotionsView };
