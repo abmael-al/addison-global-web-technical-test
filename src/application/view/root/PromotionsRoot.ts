18a895cedfe2b8dd713146f99b1a65dac8d83081
Abmael <abmael.cont@gmail.com>
2022-08-11 21:50:21 -0300

    feat: promotion component renderer

--- src/application/view/root/PromotionsRoot.ts
+++ src/application/view/root/PromotionsRoot.ts
@@ -1,5 +1,24 @@
-class PromotionsRoot {
+import { Promotion } from "../../../domain/Promotion";
 
+class PromotionsRoot {
+    private readonly ComponentBuilder;
+    private readonly root = document.querySelector('[promotions-root]') as HTMLDivElement;
+
+    constructor(ComponentBuilder: any) {
+        this.ComponentBuilder = ComponentBuilder;
+    }
+
+    get promotionsRoot() {
+        return this.root;
+    }
+
+    render(promotion: Promotion) {
+        this.root.appendChild(this.ComponentBuilder.build(promotion));
+    }
+
+    clear() {
+        this.root.replaceChildren();
+    }
 }
 
 export { PromotionsRoot };
