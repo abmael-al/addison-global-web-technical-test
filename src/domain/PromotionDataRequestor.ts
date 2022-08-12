bfcdf903d10aae7dbd6c5e3e889e2f439d7a5576
Abmael <abmael.cont@gmail.com>
2022-08-12 20:00:35 -0300

    refactor: handling error and improving request's response

--- src/domain/PromotionDataRequestor.ts
+++ src/domain/PromotionDataRequestor.ts
@@ -1,14 +1,26 @@
 import { Promotion } from './Promotion'
+import { RequestResponse } from './RequestResponse'
 import { fetch } from './http_client/fetch'
 
-// TODO: What will it return when some error ocurr ?
 class PromotionDataRequestor {
-    private URL = 'http://www.mocky.io/v2/5bc3b9cc30000012007586b7';
+    private readonly baseURL = 'http://www.mocky.io/v2';
 
-    async requestAll(): Promise<Promotion[]> {
-        const { data: promotions } = await fetch<Promotion[]>(this.URL);
-        
-        return promotions;
+    async requestAll(): Promise<RequestResponse> {
+        try {
+            const { data } = await fetch<Promotion[]>(`${this.baseURL}/5bc3b9cc30000012007586b7`);
+
+            return this.buildResponse(data);
+        }
+        catch(error: unknown) {
+            return this.buildResponse(false);
+        }
+    }
+
+    private buildResponse(result: Promotion[] | false): RequestResponse {
+        return { 
+            status: result ? 'success' : 'error',
+            result: result ? result : [],
+        };
     }
 }
 
