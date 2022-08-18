e9fe05f02802df4bb24060557df32ef73ea80ad8
Abmael <abmael.cont@gmail.com>
2022-08-17 22:01:07 -0300

    refactor: setting discriminated request response

--- src/domain/RequestResponse.ts
+++ src/domain/RequestResponse.ts
@@ -1,5 +1,23 @@
 import { Promotion } from "./Promotion";
 
+interface SuccessfulRequest {
+    // discriminated property
+    readonly status: 'success';
+    readonly result: Promotion[];
+}
+
+interface FailRequest {
+    // discriminated property
+    readonly status: 'error';
+}
+
+// Making use of discriminated unions
+type Status = SuccessfulRequest | FailRequest;
+
+interface RequestResponseRefactored {
+    readonly status: Status;
+}
+
 type RequestResponse = {
     readonly status: 'success' | 'error';
     readonly result: Promotion[];
