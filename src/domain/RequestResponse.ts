882b090d5f0fbb44432b5a398261316e39f6ee0c
Abmael <abmael.cont@gmail.com>
2022-08-31 15:32:28 -0300

    refactor: request response

--- src/domain/RequestResponse.ts
+++ src/domain/RequestResponse.ts
@@ -1,26 +1,18 @@
 import { Promotion } from "./Promotion";
 
 interface SuccessfulRequest {
-    // discriminated property
     readonly status: 'success';
     readonly result: Promotion[];
 }
 
-interface FailRequest {
-    // discriminated property
+interface FailedRequest {
     readonly status: 'error';
 }
 
-// Making use of discriminated unions
-type Status = SuccessfulRequest | FailRequest;
+type RequestResponse = SuccessfulRequest | FailedRequest;
 
-interface RequestResponseRefactored {
-    readonly status: Status;
-}
-
-type RequestResponse = {
-    readonly status: 'success' | 'error';
-    readonly result: Promotion[];
-}
-
-export type { RequestResponse };
+export type { 
+    RequestResponse, 
+    SuccessfulRequest, 
+    FailedRequest 
+};
