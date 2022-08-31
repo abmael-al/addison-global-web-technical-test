23e2b2bbefff0442e7fd2a9760463f93e39d1dce
Abmael <abmael.cont@gmail.com>
2022-08-31 18:05:06 -0300

    feat: instantiating objects, injecting dependencies and initializing view

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-export {}
+import { PromotionDataRequestor } from "./domain/PromotionDataRequestor";
+import { PromotionComponentBuilder } from "./application/components/PromotionComponentBuilder";
+import { PromotionsRenderer } from "./application/view/renderer/PromotionsRenderer";
+import { PromotionsView } from "./application/view/PromotionsView";
+import { PromotionsFilteringRequestListener } from "./application/view/listeners/promotions-filtering-request-listener";
+import { PromotionsViewStateManager } from "./application/view/state_manager/PromotionsViewStateManager";
 
-const main = ():void => {
+const main = async () => {
+    const PromotionRequestor = new PromotionDataRequestor();
+    const ComponentBuilder = new PromotionComponentBuilder();
+    const Renderer = new PromotionsRenderer(ComponentBuilder);
+    const ViewHandler = new PromotionsView(Renderer);
+    const ViewStateManager = new PromotionsViewStateManager(PromotionRequestor, ViewHandler);
+    const FilteringRequestListener = new PromotionsFilteringRequestListener(ViewStateManager);
 
+    ViewStateManager.init();
+    FilteringRequestListener.listen();
 }
 
 main();
