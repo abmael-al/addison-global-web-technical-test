1ed9a3a7aa0867599ac6b57a05e0a41cb4435e11
Abmael <abmael.cont@gmail.com>
2022-08-31 17:29:41 -0300

    refactor: removing responsibility of requesting data

--- src/application/view/PromotionsView.ts
+++ src/application/view/PromotionsView.ts
@@ -1,65 +1,21 @@
 import { Promotion } from "../../domain/Promotion";
-import { RequestResponse } from "../../domain/RequestResponse" 
 
 interface Renderer {
     render(promotion: Promotion): void;
     clear(): void;
 }
 
-interface Requestor {
-    requestAll(): Promise<RequestResponse>;
-    requestPromotionsForAllCustomers(): Promise<RequestResponse>;
-    requestPromotionsForNewCustomers(): Promise<RequestResponse>;
-}
-
 class PromotionsView {
-    private readonly PromotionsRequestor;
     private readonly PromotionsRenderer;
 
-    constructor(PromotionsRequestor: Requestor, PromotionsRenderer: Renderer) {
-        this.PromotionsRequestor = PromotionsRequestor;
+    constructor(PromotionsRenderer: Renderer) {
         this.PromotionsRenderer = PromotionsRenderer;
     }
-    
-    async start() {
-        const response = await this.PromotionsRequestor.requestAll();
 
-        if(response.status === 'success') {
-            this.render(response.result);
-        }
-    }
-
-    private render(promotions: Promotion[]) {
-        promotions.sort();
-
+    display(promotions: Promotion[]) {
         for(const promotion of promotions){
             this.PromotionsRenderer.render(promotion);
         }
-    }
-
-    async renderNewBatch(filter?: 'new-customers' | 'all-customers') {
-        let response;
-
-        if(filter) {
-            const filteringHandlers = {
-                'new-customers': () => this.PromotionsRequestor.requestPromotionsForNewCustomers(),
-                'all-customers': () => this.PromotionsRequestor.requestPromotionsForAllCustomers()
-            }
-
-            response = await filteringHandlers[filter]();
-        }
-        else {
-            response = await this.PromotionsRequestor.requestAll();
-        }
-
-        if(response.status === 'success') {
-            this.clear();
-            this.render(response.result);
-        }
-    }
-
-    private clear() {
-        this.PromotionsRenderer.clear();
     }
 }
 
