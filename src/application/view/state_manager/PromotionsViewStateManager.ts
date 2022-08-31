d286a2627fc4fcd9ade48d0d61fb7ef4ec992b17
Abmael <abmael.cont@gmail.com>
2022-08-31 17:21:46 -0300

    feat: promotion view state manager

---
+++ src/application/view/state_manager/PromotionsViewStateManager.ts
@@ -0,0 +1,51 @@
+import { Promotion } from "../../../domain/Promotion";
+import { RequestResponse } from "../../../domain/RequestResponse";
+
+interface Requestor {
+    requestAll(): Promise<RequestResponse>;
+    requestPromotionsForAllCustomers(): Promise<RequestResponse>;
+    requestPromotionsForNewCustomers(): Promise<RequestResponse>;
+}
+
+interface View {
+    display(promotions: Promotion[]): void;
+    clear(): void;
+}
+
+type Filtering = 'new-customers' | 'all-customers';
+
+class PromotionsViewStateManager {
+    private readonly PromotionsRequestor;
+    private readonly ViewHandler;
+
+    constructor(PromotionsRequestor: Requestor, ViewHandler: View) {
+        this.PromotionsRequestor = PromotionsRequestor;
+        this.ViewHandler = ViewHandler;
+    }
+
+    async init() {
+        const response = await this.PromotionsRequestor.requestAll();
+
+        if(response.status === 'success') {
+            this.ViewHandler.display(response.result.sort());
+        }
+    }
+
+    async rerender(filtering?: Filtering) {
+        const filteringHandler = {
+            'new-customers': () => this.PromotionsRequestor.requestPromotionsForNewCustomers(),
+            'all-customers': () => this.PromotionsRequestor.requestPromotionsForAllCustomers(),
+        }
+
+        const response = (filtering) 
+            ? await filteringHandler[filtering]() 
+            : await this.PromotionsRequestor.requestAll();
+
+        if(response.status === 'success') {
+            this.ViewHandler.clear();
+            this.ViewHandler.display(response.result.sort());
+        }
+    }
+}
+
+export { PromotionsViewStateManager }
