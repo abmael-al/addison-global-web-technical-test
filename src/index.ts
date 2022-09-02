a3f4ebe8ef30e14df725c304a23b2a5b7fb42989
Abmael <abmael.cont@gmail.com>
2022-09-02 17:21:19 -0300

    refactor: renaming file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { PromotionDataRequestor } from "./domain/PromotionDataRequestor";
 import { PromotionComponentBuilder } from "./application/components/PromotionComponentBuilder";
 import { PromotionsRenderer } from "./application/view/renderer/PromotionsRenderer";
 import { PromotionsView } from "./application/view/PromotionsView";
-import { PromotionsFilteringRequestListener } from "./application/view/listeners/promotions-filtering-request-listener";
+import { PromotionsFilteringRequestListener } from "./application/view/listeners/PromotionsFilteringRequestListener";
 import { PromotionsViewStateManager } from "./application/view/state_manager/PromotionsViewStateManager";
 
 const main = async () => {
